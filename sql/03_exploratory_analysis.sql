3aaa8ce12d9a1ce014b34919a96593d95fbc2d8f
jsoncruzsipiran <119018676+jsoncruzsipiran@users.noreply.github.com>
2026-08-29 19:22:14 -0400

    Queried to find the players who've participated in games at the highest elevated stadiums for the 2026 world cup

--- sql/03_exploratory_analysis.sql
+++ sql/03_exploratory_analysis.sql
@@ -1,60 +1,57 @@
--- DISPLAY ENTIRE TABLE
-SELECT * FROM world_cup_2026_matches;
+-- DISPLAY OF TABLES
+SELECT * FROM match_events;
+SELECT * FROM match_lineups;
+SELECT * FROM match_team_stats;
+SELECT * FROM matches_detailed;
+SELECT * FROM matches;
+SELECT * FROM player_stats;
+SELECT * FROM referees;
+SELECT * FROM squads_and_players;
+SELECT * FROM teams;
+SELECT * FROM tournament_stages;
+SELECT * FROM venues;
 
-/* WINNERS FOR EACH MATCH | ONLY 84 MATCHES HAD A WINNER */
-WITH away_winners AS ( -- All instances of teams who have won their away games
+
+/* Did players, who come from countries that naturally have high elevations, perform better to other players during matches in highly-elevated stadiums? */
+
+-- find matches played in high elevation stadiums
+WITH matches_with_high_elevations AS (
     SELECT
-        away_team AS match_winner,
-        score,
-        notes
-    FROM 
-        world_cup_2026_matches
-    WHERE
-        away_score > home_score
-        OR
-        -- KNOCKOUT STAGE MATCHES HAD NO VALUES FOR away_score & home_score, notes CONTAINED MATCH WINNER
-        notes ILIKE '%' || away_team || '%'
-            -- ILIKE finds pattern, regardless of capitalization
-            -- || concats strings --> %away_team%
+        matches_detailed.match_id,
+        matches_detailed.stadium_name,
+        venues.elevation_meters,
+        matches_detailed.home_team_name,
+        matches_detailed.away_team_name,
+        matches_detailed.home_score,
+        matches_detailed.away_score,
+        matches_detailed.home_xg,
+        matches_detailed.away_xg
+    FROM matches_detailed
+    JOIN venues ON matches_detailed.stadium_name = venues.stadium_name
+    WHERE elevation_meters >= 1200
 ),
-home_winners AS ( -- All instances of teams who have won their home games
-    SELECT
-        home_team AS match_winner,
-        score,
-        notes
-    FROM 
-        world_cup_2026_matches
-    WHERE
-        home_score > away_score
-        OR
-        -- KNOCKOUT STAGE MATCHES HAD NO VALUES FOR away_score & home_score, notes CONTAINED MATCH WINNER
-        notes IlIKE '%' || home_team || '%'
-            -- ILIKE finds pattern, regardless of capitalization
-            -- || concats strings --> %home_team%
 
+-- find the names + teams for each match in the world cup
+relevant_match_lineups AS (
+    SELECT
+        match_lineups.match_id,
+        player_stats.player_name,
+        teams.team_name
+    FROM match_lineups
+    JOIN player_stats ON match_lineups.player_id = player_stats.player_id
+    JOIN teams ON match_lineups.team_id = teams.team_id
+    WHERE match_lineups.minutes_played > 0
 )
 
--- Table for all match winners
+-- combine tables to find the players who've played matches in the most elevated stadiums at the 2026 world cup
 SELECT
-    *
-FROM
-    home_winners
+    relevant_match_lineups.*,
+    matches_with_high_elevations.*
+FROM relevant_match_lineups
+JOIN matches_with_high_elevations ON relevant_match_lineups.match_id = matches_with_high_elevations.match_id
+ORDER BY 
+    relevant_match_lineups.match_id, 
+    team_name;
 
-UNION ALL
-
-SELECT
-    *
-FROM
-    away_winners;
-
-/* GAMES WITH NO DECIDED WINNER (TIED GROUP STAGE MATCHES) || 20 MATCHES WITH NO DECIDED WINNER */
-SELECT
-    home_team,
-    away_team,
-    score
-FROM
-    world_cup_2026_matches
-WHERE
-    home_score = away_score;
-
-/* ALL 104 GAMES ARE ACCOUNTED FOR */
+/* DATA DISPLAYS LIST OF PLAYERS WHO'VE PARTICIPATED IN THESE MATCHES OF INTEREST */
+/* MUST EXPLORE TO FIND INDIVIDUAL PLAYER PERFORMANCE STATISTICS */
